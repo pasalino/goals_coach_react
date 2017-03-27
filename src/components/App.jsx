032a068bb09388df61d5edb8286b0b63690e081a
Pasqualino de Simone <pasalino@MacBook-Pro-di-Pasqualino.local>
2017-03-27 10:01:09 +0200

    Remove unused dependeces

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firebaseApp } from '../firebase';
-import Confirm from 'react-bootstrap-confirm'
 import AddGoals from './AddGoals';
 import GoalsList from './GoalsList';
 import CompletedGoalsList from './CompletedGoalList';
